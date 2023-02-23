9a649e3897d2121c7881d549929cb401cf89f0ab
kharl112 <kharl.dms112@gmail.com>
2023-02-23 16:26:17 +0800

    changed views

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const { sequelize } = require("./dbConnection");
+const express = require("express");
 const app = require("./src/routes/app");
 
 require("dotenv").config()
 const PORT = process.env.PORT || 5000;
+
+const path = require("path");
+
+app.use(express.static("views"));
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/index.html"));
+});
 
 sequelize.authenticate().then(() => {
     console.log("connected to the database");

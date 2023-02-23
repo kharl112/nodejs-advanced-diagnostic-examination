d4f018cc09e024ef1317242219b6383146982fea
kharl112 <kharl.dms112@gmail.com>
2023-02-23 08:04:29 +0800

    assertion calss in profile.test.js modified

--- __tests__/user/profile.test.js
+++ __tests__/user/profile.test.js
@@ -7,13 +7,16 @@ describe('GET /api/user/profile', () => {
     const url = "/api/user/profile"
 
     it('tests user profile', async () => {
-        expect.assertions(1);
         try {
+            expect.assertions(3);
             const user = await request(app).post("/api/user/login").send(loginPayload);
             const response = await request(app).get(url).set({ Authorization: user.body.token });
 
             expect(response.body).toHaveProperty('id');
+            expect(response.body.username).toMatch(loginPayload.username);
+            expect(response.body.username).toMatch(loginPayload.username);
         } catch (error) {
+            expect.assertions(1);
             expect(error).toHaveProperty('message')
         }
     });

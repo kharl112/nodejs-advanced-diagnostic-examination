08d3ae9bd2a60e0bc1c1df24b12a5cd229c59500
kharl112 <kharl.dms112@gmail.com>
2023-02-23 09:37:39 +0800

    user_get_all test created

---
+++ __tests__/admin/user_get_all.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require("../../src/routes/app");
+require("dotenv").config()
+
+const loginPayload = { username: process.env.ADMIN_USERNAME, password: process.env.ADMIN_PASSWORD };
+
+describe(`GET /api/admin/users/`, () => {
+    const url = `/api/admin/users/`
+
+    it('get all users', async () => {
+        try {
+            expect.assertions(2);
+            const admin = await request(app).post("/api/admin/login").send(loginPayload);
+            const response = await request(app).get(url).set({ Authorization: admin.body.token });
+            expect(Array.isArray(response.body)).toBeTruthy();
+            expect(!!response.body.length).toBeTruthy();
+        } catch (error) {
+            expect.assertions(1);
+            expect(error).toHaveProperty('message')
+        }
+    });
+});
